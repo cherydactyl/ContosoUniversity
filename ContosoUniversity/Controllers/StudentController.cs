371a261c351de27a02a86e114c1ade8691b1f8c2
cherydactyl <cheryl.orosz@gmail.com>
2015-08-10 10:38:44 -0400

    Update Details controller method
    Added code to Details controller method in
    Controllers\StudentController.cs

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -25,14 +25,14 @@ namespace ContosoUniversity.Controllers
         public ActionResult Details(int? id)
         {
             if (id == null)
-            {
+                {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+                }
             Student student = db.Students.Find(id);
             if (student == null)
-            {
-                return HttpNotFound();
-            }
+                {
+                    return HttpNotFound();
+                }
             return View(student);
         }
 
