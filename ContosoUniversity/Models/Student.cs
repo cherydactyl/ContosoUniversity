a40dcb9545eee37c71c28f20e15711a70d5e4d15
cherydactyl <cheryl.orosz@gmail.com>
2015-08-12 11:12:28 -0400

    Updated model Student

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -8,16 +8,32 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
         [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Last must contain only letters and start with a capital letter.")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "First must contain only letters and start with a capital letter.")]
-        //[Column("FirstName")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
